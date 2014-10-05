8bed8a62112fa069592c41313dcc79d9e88e58f6
Jacob Chapel <jacob.chapel@gmail.com>
2014-10-05 11:50:27 -0700

    Add styles from custom

--- plugin.rb
+++ plugin.rb
@@ -6,3 +6,9 @@
 ### Large avatars
 register_asset "stylesheets/large-avatars.css.scss", :desktop
 register_asset "javascripts/initializers/large-avatars.js.es6"
+
+### Large posts
+register_asset "stylesheets/large-posts.css.scss", :desktop
+
+### Color modifications
+register_asset "stylesheets/colors.css.scss"
