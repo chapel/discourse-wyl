fc49e5bc0345125ae8ab74822580a4f187434ebe
Jacob Chapel <jacob.chapel@gmail.com>
2019-02-01 03:14:10 -0800

    Clean up unused files

--- plugin.rb
+++ plugin.rb
@@ -3,16 +3,8 @@
 # version: 1.0
 # authors: Jacob Chapel
 
-### Large avatars
-# register_asset "stylesheets/large-avatars.css.scss", :desktop
-# register_asset "javascripts/discourse/templates/post/poster-avatar.raw.hbs"
-
 ### Large posts
 register_asset "stylesheets/large-posts.css.scss", :desktop
 
 ### Color modifications
 register_asset "stylesheets/colors.css.scss"
-
-### Spoiler post toolbar button
-#register_asset "javascripts/spoiler-toolbar.js", :composer
-#register_asset "stylesheets/spoiler-toolbar.css.scss"
